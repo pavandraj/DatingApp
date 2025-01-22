211453248fc1e68991dc444089d97ac412c5c58a
Pavan Kumar D <pavan.d@scientificgames.com>
2025-01-22 14:31:29 +0530

    first commit UI

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("MyConnection"));
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
