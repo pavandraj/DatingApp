a1e899613abd7965c9e667c2efdabd53c2af4c14
Pavan Kumar D <pavan.d@scientificgames.com>
2025-01-22 15:15:02 +0530

    database new column

--- API/Entities/AppUsers.cs
+++ API/Entities/AppUsers.cs
@@ -8,4 +8,6 @@ public class AppUsers
     [Key]
     public int Id { get; set; }
     public required string UserName { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
